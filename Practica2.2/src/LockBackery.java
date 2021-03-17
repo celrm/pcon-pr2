ad672d782ae0b986f3cc676224228bc4402bae55
Celia Rubio Madrigal <celia_rubio@hotmail.es>
2021-03-17 13:20:02 +0100

    backery finish

--- Practica2.2/src/LockBackery.java
+++ Practica2.2/src/LockBackery.java
@@ -4,30 +4,25 @@ import java.util.function.IntBinaryOperator;
 public class LockBackery implements Lock {
 
 	volatile AtomicInteger[] turn;
-	volatile boolean[] max;
-	
+
 	public LockBackery(int M) {
 		this.turn = new AtomicInteger[2*M+1];
-		this.max = new boolean[2*M+1];
 		for(int j = 1; j < turn.length; ++j) {
 			turn[j] = new AtomicInteger(0);
-			max[j] = false;
 		}
 	}
 	
 
 	public void takeLock(int id) {
-		max[id]=true;
+		turn[id].set(1);
 		int mx = 0;
 		for(int j = 1; j < turn.length; ++j) {
 			mx = Math.max(mx,turn[j].get());
 		}
 		turn[id].set(mx+1);
-		max[id]=false;
 
 		for(int j = 1; j < turn.length; ++j) {
 			if(j!=id) {
-				while(max[j]);
 				while(turn[j].get()!=0 && 
 					comparing(turn[id].get(),id,turn[j].get(),j));
 			}
